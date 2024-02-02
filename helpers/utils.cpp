15ad431fcd661fedae2585bd0301bbc2e827defa
Dpbm <dpbm136@gmail.com>
2024-02-02 11:37:35 -0300

    added random_int

--- helpers/utils.cpp
+++ helpers/utils.cpp
@@ -17,8 +17,16 @@ namespace Utils {
     return distr(eng);
   }
   
+  unsigned int random_int(int start, int end){
+    std::random_device device;
+    std::mt19937 eng(device());
+    std::uniform_int_distribution<> distr(start, end);
+    
+    return distr(eng);
+  }
+
   unsigned int get_random_pos(unsigned int max_range, unsigned int factor){
-    unsigned int rand = Utils::random(0, max_range);
+    unsigned int rand = Utils::random_int(0, max_range);
     return rand-(rand%factor);
   }
 
--- helpers/utils.h
+++ helpers/utils.h
@@ -7,6 +7,7 @@ using std::string;
 namespace Utils {
   unsigned int get_random_pos(unsigned int max_range, unsigned int factor);
   double random(int start, int end);
+  unsigned int random_int(int start, int end);
   bool passed_debounce_time(int last_tick);
   void append_to_file(string filename, string data);
   void create_file(string filename, string data);
