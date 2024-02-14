18fd91eb42ff915bbd80705980cddfbef18e18bb
Dpbm <dpbm136@gmail.com>
2024-02-13 21:55:48 -0300

    added text

---
+++ dep_opensuse.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+sudo zypper install libSDL2-2_0-0 \
+  libSDL2-devel \
+  cmake \
+  make \
+  libSDL2_ttf-2_0-0 \
+  libSDL2_ttf-devel \
+  pattern \
+  clang \
+  clang-tools 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_surface.h>
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_ttf.h>
 #include "helpers/constants.h"
 #include "game/player.h"
 #include "game/food.h"
@@ -16,6 +19,7 @@ using Players::Player;
 int main(){
   //TODO: Check for SDL errors
   SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
   SDL_Window* window = SDL_CreateWindow(
     "snake game", 
     SDL_WINDOWPOS_UNDEFINED, 
@@ -27,10 +31,31 @@ int main(){
   SDL_Event *event = new SDL_Event;
   Food *food = new Food;
 
+  bool playing = true;
   unsigned int max_score = 1000;
   Player* player = new Player(10, max_score);
+  
+  TTF_Font* text_font = TTF_OpenFont("./assets/roboto.ttf", 16);
+  if(!text_font){
+    cout << "Failed on getting font!" << endl;
+    cout << TTF_GetError() << endl;
+    return 1;
+  }
 
-  while(true){
+  SDL_Color Foreground = { 255, 255, 255 };
+  SDL_Color Background = { 0, 0, 0 };
+  
+  SDL_Surface* text_surface = TTF_RenderText_Solid(text_font, "Hello world!", Foreground);
+  SDL_Texture* text_texture = SDL_CreateTextureFromSurface(render, text_surface);
+  SDL_FreeSurface(text_surface);
+
+  SDL_Rect* text_shape = new SDL_Rect;
+  text_shape->x = 10;
+  text_shape->y = 10;
+  text_shape->h = 200;
+  text_shape->w = 200;
+
+  while(playing){
     SDL_PollEvent(event);
 
     if(event->type == SDL_QUIT) 
@@ -53,10 +78,14 @@ int main(){
           player->direction_left();
           break;
 
+        case SDLK_ESCAPE:
+          playing = false;
+          break;
+        
         default: break;
       }
     }    
-    
+
     player->update_position();
    
 
@@ -75,6 +104,7 @@ int main(){
     }
 
     SDL_RenderClear(render);
+    SDL_RenderCopy(render, text_texture, NULL, text_shape);
     food->render(render);
     player->render(render);
     SDL_RenderPresent(render);
@@ -82,10 +112,13 @@ int main(){
 
   SDL_DestroyWindow(window);
   SDL_DestroyRenderer(render);
+  SDL_DestroyTexture(text_texture);
   SDL_Quit();
+  TTF_Quit();
   delete event;
   delete food;
   delete player;
+  delete text_shape;
 
   return 0;
 }
