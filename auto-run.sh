b166808f225de82271333a7f66d8b0c8c7f6e049
Dpbm <dpbm136@gmail.com>
2024-02-14 11:19:53 -0300

    added script to run all the workflow

---
+++ auto-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/sh
+
+set -e
+
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+chmod +x ./clean.sh
+chmod +x ./compile.sh
+
+printf "${GREEN}Cleaning directories...${ENDCOLOR}"
+./clean.sh
+printf "\n\n"
+
+printf "${GREEN}Compiling: 'manual_tests.cpp'${ENDCOLOR}\n"
+./compile.sh manual_tests.cpp
+printf "\n\n"
+
+printf "${GREEN}Running: 'manual_tests'${ENDCOLOR}\n"
+./build/ga.out
+printf "\n\n"
+
+printf "${GREEN}Compiling: 'main.cpp'${ENDCOLOR}\n"
+./compile.sh main.cpp
+printf "\n\n"
+
+
+printf "${GREEN}Running tests${ENDCOLOR}\n"
+cd build && ctest -V
+printf "\n\n"
