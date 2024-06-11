0b142568a208d586eb279769a153494bdb361e33
Dpbm <dpbm136@gmail.com>
2024-06-11 16:04:02 -0300

    added distance test

--- tests/utils_test.cc
+++ tests/utils_test.cc
@@ -1,6 +1,22 @@
 #include <gtest/gtest.h>
+#include "../helpers/utils.h"
 
+using Utils::distance;
 
 namespace {
+  TEST(ValueTest, ZeroDistanceTest){
+    ASSERT_EQ(distance(1, 1, 1, 1), 0);
+  }
   
+  TEST(ValueTest, AllZerosDistanceTest){
+    ASSERT_EQ(distance(0, 0, 0, 0), 0);
+  }
+  
+  TEST(ValueTest, ZeroDistanceNegativeDistanceTest){
+    ASSERT_EQ(distance(-1, -2, -1, -2), 0);
+  }
+  
+  TEST(ValueTest, GetDistanceTest){
+    ASSERT_EQ(distance(1, 4, 5, 1), 5);
+  }
 }
